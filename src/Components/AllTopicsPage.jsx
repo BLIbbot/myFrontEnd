f5d208f5e3619e750d75507e4451fec68c1753af
BLIbbo <BLIbbotson99@gmail.com>
2024-11-08 21:20:46 +0000

    Stage 7 - Topics page and individual topics pages

---
+++ src/Components/AllTopicsPage.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import { getAllTopics } from "../../api";
+import { Link, useSearchParams } from "react-router-dom";
+
+export const AllTopicsPage = () => {
+  const [topics, setTopics] = useState([]);
+
+  useEffect(() => {
+    getAllTopics().then((topics) => {
+      setTopics(topics);
+    });
+  }, []);
+
+  return (
+    <ul>
+      {topics.map((topic) => {
+        return (
+          <Link className="link" to={`/articles?topic=${topic.slug}`}>
+            <li className="TopicLinks" key={topic.slug}>
+              {topic.slug} (copy to clipboard)
+            </li>
+          </Link>
+        );
+      })}
+    </ul>
+  );
+};
