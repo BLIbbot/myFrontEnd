9c0755db7d50b138fbf6c772df6c18f408b39d18
BLIbbo <BLIbbotson99@gmail.com>
2024-11-08 16:00:13 +0000

    Stage 6 - Comments can now be deleted by signed in users

--- src/Components/CommentsSection.jsx
+++ src/Components/CommentsSection.jsx
@@ -1,6 +1,7 @@
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
 import { useState } from "react";
-import { getComments } from "../../api";
+import { deleteComment, getComments } from "../../api";
+import { UserContext } from "./UserContext";
 
 const CommentsSection = ({ article_id }) => {
   const [comments, setComments] = useState([]);
@@ -11,15 +12,26 @@ const CommentsSection = ({ article_id }) => {
     });
   }, [comments]);
 
+  const removeComment = (comment) => {
+    deleteComment(comment);
+  };
+
+  const { user } = useContext(UserContext);
+
   return (
     <ul>
       {comments.map((comment) => {
         return (
-          <li key={comment.created_at} id="CommentCard">
+          <li key={comment.comment_id} id="CommentCard">
             <h2>{comment.author}</h2>
             <p>{comment.body}</p>
             <p>Votes: {comment.votes}</p>
-            <p>ID: {comment.article_id}</p>
+            <p>ID: {comment.comment_id}</p>
+            {comment.author === user ? (
+              <button onClick={() => removeComment(comment.comment_id)}>
+                Delete Comment
+              </button>
+            ) : null}
           </li>
         );
       })}
