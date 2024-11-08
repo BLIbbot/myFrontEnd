cd8a68430716ffbc21deb0c9a5aac3f3ca1f5489
BLIbbo <BLIbbotson99@gmail.com>
2024-11-08 13:08:11 +0000

    Stage 4 - articles can be up/downvoted with no limit

--- src/Components/IndividualArticlePage.jsx
+++ src/Components/IndividualArticlePage.jsx
@@ -1,25 +1,45 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { getArticleByID } from "../../api";
+import { addVote, getArticleByID } from "../../api";
 import CommentsSection from "./CommentsSection";
 
 const IndividualArticlePage = () => {
   const [article, setArticle] = useState({});
+  const [votes, setVotes] = useState(0);
 
   const { article_id } = useParams();
   useEffect(() => {
     getArticleByID(article_id).then((article) => {
       setArticle(article.article[0]);
+      setVotes(article.article[0].votes);
     });
   }, []);
+
+  const vote = (value) => {
+    setVotes((currentvotes) => {
+      return currentvotes + value;
+    });
+    addVote(article_id, value)
+      .then(() => {})
+      .catch(() => {
+        setVotes((currentvotes) => {
+          return currentvotes - value;
+        });
+      });
+  };
 
   return (
     <>
       <section className="IndividualArticle">
         <h1 id="ArticleTitle">{article.title}</h1>
         <img id="ArticleImg" src={article.article_img_url} />
-        <p id="ArticleVotes">Votes:{article.votes}</p>
-        <button id="AddVoteButton">Add Vote</button>
+        <p id="ArticleVotes">Votes:{votes}</p>
+        <button id="AddVoteButton" onClick={() => vote(1)}>
+          UpVote
+        </button>
+        <button id="RemoveVoteButton" onClick={() => vote(-1)}>
+          DownVote
+        </button>
         <h3 id="TopComment"></h3>
         <p id="ArticleTopic">Topic: {article.topic}</p>
         <p id="ArticleAuthor">Authored by: {article.author}</p>
@@ -27,7 +47,7 @@ const IndividualArticlePage = () => {
           Created at: {new Date(article.created_at).toLocaleString()}
         </p>
       </section>
-      <CommentsSection article_id={article_id} id="commentssection" />
+      <CommentsSection article_id={article_id} />
     </>
   );
 };
